123782d2e4c9cdafda840e302e2eb32a8d3e73f2
Felipo <gabriel.felipo2010@gmail.com>
2024-07-13 12:36:56 -0300

    feature(citizen-schedule): add use-case, repository, entity and dto to schedule module

---
+++ src/modules/schedule/repositories/schedule.repository.ts
@@ -0,0 +1,12 @@
+import { Injectable } from "@nestjs/common";
+import { Schedule } from "../entities/Schedule";
+
+@Injectable()
+export abstract class ScheduleRepository{
+
+    abstract create(schedule: Schedule): Promise<Schedule>
+
+    abstract findById(id: string): Promise<Schedule | null>
+
+    abstract countSchedules(date: Date, hour: Date): Promise<number>
+}
